08dd2c02cfd921c5a16bfd0895f13c0fa5d097ba
skarn3 <skarn@emich.edu>
2024-04-18 16:16:46 -0400

    a fetching function is called everytime ID is changedd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import './App.css'; // Import your CSS file
 
 function App() {
   const [movieId, setMovieId] = useState('');
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [error, setError] = useState(null);
 
 
   const handleMovieIdChange = (event) => {
@@ -12,6 +14,29 @@ function App() {
   };
 
 
+  useEffect(() => {
+    if (movieId.trim() !== '') {
+      fetch(`http://localhost:8000/movies/${movieId}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Movie not found');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          setMovieDetails(data);
+          setError(null);
+        })
+        .catch(error => {
+          setMovieDetails(null);
+          setError('Movie not found');
+        });
+    } else {
+      setMovieDetails(null);
+      setError(null);
+    }
+  }, [movieId]);
 
   return (
     <div className="App">
